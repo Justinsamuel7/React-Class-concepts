e91397950b4e128f734e69b4630a132e21ff34a2
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-02-22 12:36:01 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React,{Component} from "react";
 import "./style.css";
+import {useState} from "react";
+
 
 export default class Just extends Component{
 
@@ -8,13 +10,18 @@ export default class Just extends Component{
     super(props);
 
   }
+  // nameprint(){
+  //   const [name,setname]=useState('Justin')
+  //   setname("justinsam")
+  // }
+
 
   render() {
+    
     return (
       <div>
         <h4>Child component </h4>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-       
       </div>
     );
   }
